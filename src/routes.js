960565d29ce6881f07a7ac195c462149b8605b6b
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-05-13 13:05:09 -0300

    model de usuario

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,21 @@
 import { Router } from "express"
+import { v4 } from 'uuid'
+
+import User from './app/models/User'
 
 const routes = new Router()
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "hello world" })
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Jacquin',
+    email: 'jacquin2@gmail.com',
+    password_hash: '123abc456',
+    admin: true,
+
+  })
+
+  return response.json(user)
 })
 
 export default routes
