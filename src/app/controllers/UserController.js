a7c104d02ce700f973b8a14e83db324cc3c6ac0a
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-05-16 20:58:14 -0300

    validando email duplicado

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,11 +1,36 @@
 
 import { v4 } from 'uuid'
-import User from '../models/User'
+import * as Yup from 'yup'
 
+import User from '../models/User'
 class UserController{
    async store(request,response){
 
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().required().min(6),
+            admin: Yup.boolean(),
+
+        })
+
+        try{
+            await schema.validateSync(request.body, { abortEarly: false})
+        }catch(err){
+            return response.status(400).json({error: err.errors})
+        }
+
+       
+
         const { name, email, password_hash, admin } = request.body 
+
+        const userExist = await User.findOne({
+            where: { email },
+        })
+
+        if(userExist){
+            return response.status(400).json({ error:"User alredy exists" })
+        }
 
     const user =   await User.create({
         id: v4(),
