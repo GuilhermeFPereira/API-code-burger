b51e4fb70fd89b26d32a55bbac341e0170e0748b
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-05-16 14:52:16 -0300

    criando crontroller de usuario

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,9 @@
 import { Router } from "express"
-import { v4 } from 'uuid'
 
-import User from './app/models/User'
+import UserController from "./app/controllers/UserController"
 
 const routes = new Router()
 
-routes.get('/', async (request, response) => {
-  const user = await User.create({
-    id: v4(),
-    name: 'Jacquin',
-    email: 'jacquin2@gmail.com',
-    password_hash: '123abc456',
-    admin: true,
-
-  })
-
-  return response.json(user)
-})
+routes.post('/users', UserController.store )
 
 export default routes
