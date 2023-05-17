1f9b42d36d180abbca1415eff0efe09ead3f224b
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-05-17 20:47:22 -0300

    configurando token jwt

--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,16 @@ import SessionController from "./app/controllers/SessionController"
 import ProductController from "./app/controllers/ProductController"
 
 
+
+import authMiddleware from './app/middlewares/auth'
 const upload = multer(multerConfig)
 const routes = new Router()
 
 routes.post('/users', UserController.store )
 
 routes.post('/sessions', SessionController.store )
+
+routes.use(authMiddleware) // todas as rotas a BAIXO serao chamados pelo Middleware
 
 routes.post('/products', upload.single('file'), ProductController.store )
 routes.get('/products', ProductController.index )
