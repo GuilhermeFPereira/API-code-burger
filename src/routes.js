b3ceefb780b7b2a2373d33bcc39a1ddbed428271
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-05-17 21:36:33 -0300

    criacao da tabela de categorias

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer'
 import UserController from "./app/controllers/UserController"
 import SessionController from "./app/controllers/SessionController"
 import ProductController from "./app/controllers/ProductController"
+import CategoryController from "./app/controllers/CategoryController"
 
 
 
@@ -20,5 +21,8 @@ routes.use(authMiddleware) // todas as rotas a BAIXO serao chamados pelo Middlew
 
 routes.post('/products', upload.single('file'), ProductController.store )
 routes.get('/products', ProductController.index )
+
+routes.post('/categories', CategoryController.store )
+routes.get('/categories', CategoryController.index )
 
 export default routes
