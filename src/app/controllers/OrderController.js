cfb31c2c3eba3dc17f188ac212259686884afa15
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-05-18 21:24:17 -0300

    validando admin

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -3,6 +3,7 @@ import * as Yup from 'yup'
 import Product from '../models/Product'
 import Category from '../models/Category'
 import Order from '../schemas/Order'
+import User from '../models/User'
 
 class OrderController{
    async store(request,response){
@@ -89,6 +90,10 @@ class OrderController{
             return response.status(400).json({error: err.errors})
         }
 
+        const { admin: isAdmin} = await User.findByPk(request.userId)
+        if(!isAdmin){
+            return response.status(401).json()
+        }
 
         const { id } = request.params
         const { status } = request.body
